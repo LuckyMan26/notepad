96e9538c7b486037b63030680082ad0ec482c0f2
LuckyMan26 <volikartem6@gmail.com>
2023-04-19 20:07:28 +0300

    Setting up

--- notepadMultiThreaded/main.cpp
+++ notepadMultiThreaded/main.cpp
@@ -2,10 +2,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <iostream>
 
 
 int main(int argc, char *argv[])
 {
+    std::cout<<"Hello"<<std::endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
